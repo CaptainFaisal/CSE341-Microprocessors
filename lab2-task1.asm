5b3bb2a11d3332cda0316ac5c94b3f2ee19ce0a9
CaptainFaisal <faisalhawlader04@gmail.com>
2024-02-29 17:04:06 +0600

    Create lab2-task1.asm

---
+++ lab2-task1.asm
@@ -0,0 +1,29 @@
+data segment
+    disp db "Please insert a character: $"
+ends
+
+code segment
+    start:
+    
+    lea dx, disp
+    mov ah, 9
+    int 21h ; Showing prompt
+    
+    mov ah, 1
+    int 21h ; Taking input
+    
+    mov cl, al ; Saving the input
+    
+    mov ah, 2
+    mov dl, 0dh
+    int 21h
+    mov dl, 0ah
+    int 21h ; CRLF
+    
+    mov dl, cl
+    int 21h ; Showing the character
+    
+        
+    exit:
+    end start
+ends
