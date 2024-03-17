f8025ae3bddd56f876dc9711d99a9eea1ba7bca9
CaptainFaisal <faisalhawlader04@gmail.com>
2024-03-17 14:47:53 +0600

    Update task10.asm

--- lab3/task10.asm
+++ lab3/task10.asm
@@ -1,7 +1,7 @@
 data segment
     input db "Input 3 number (One digit each): $"
-    max db " is max$"
-    min db " is min$"
+    max db "Maximum number is $"
+    min db "Minimum number is $"
 ends
 
 code segment
@@ -38,27 +38,27 @@ code segment
     jmp maxB
  
     maxA:
+    mov ah, 9
+    lea dx, max
+    int 21h
     mov ah, 2
     mov dl, ch
     int 21h
+    jmp minStart
+    maxB:
     mov ah, 9
     lea dx, max
     int 21h
-    jmp minStart
-    maxB:
     mov ah, 2
     mov dl, cl
     int 21h
-    mov ah, 9
-    lea dx, max
-    int 21h
     jmp minStart
     maxC:
-    mov ah, 2
-    mov dl, bl
-    int 21h
     mov ah, 9
     lea dx, max
+    int 21h
+    mov ah, 2
+    mov dl, bl
     int 21h
     
     minStart:
@@ -79,27 +79,27 @@ code segment
     jmp minB
     
     minA:
+    mov ah, 9
+    lea dx, min
+    int 21h
     mov ah, 2
     mov dl, ch
     int 21h
+    jmp exit
+    minB:
     mov ah, 9
     lea dx, min
     int 21h
-    jmp exit
-    minB:
     mov ah, 2
     mov dl, cl
     int 21h
-    mov ah, 9
-    lea dx, min
-    int 21h
     jmp exit
     minC:
-    mov ah, 2
-    mov dl, bl
-    int 21h
     mov ah, 9
     lea dx, min
+    int 21h
+    mov ah, 2
+    mov dl, bl
     int 21h
     exit:
     end start
